8fba2b5a11ccce91909ab23040cafe91e966e65d
Hiroki Miyabayashi <Hiroki.miya00@gmail.com>
2021-02-24 16:15:17 -0500

    sleep_in written

---
+++ warmup-1/sleep_in/sleep_in.py
@@ -0,0 +1,11 @@
+'''
+the Warmup-1/sleep_in problem from Coding Bat
+'''
+def sleep_in(weekday, vacation):
+    ''' 
+    Takes two parameters that indicate if today is a weekday,
+    and whether or not we are currently on vacation. Depending,
+    on those values, it outputs True/False to let us know if
+    we can/should sleep in today.
+    '''
+    return True # NOT the actual code; placeholder so our tests will run
--- warmup-1/sleep_in/sleep_in_test.py
+++ warmup-1/sleep_in/sleep_in_test.py
@@ -1,11 +1,11 @@
 '''
-Tests for the sleep_in function
+Tests for the sleep_in function for [this Coding Bat problem](https://codingbat.com/prob/p173401)
 '''
 
 # import the code to be tested
 from sleep_in import sleep_in
 
-# begin writing tests
+# begin writing tests (top-level test suite)
 def describe_a_sleep_in_function():
 
     def that_determines_whether_or_not_we_should_sleep_in():
@@ -15,7 +15,7 @@ def describe_a_sleep_in_function():
         2 values each, means we have a total of (2 x 2 = 4) 4 possibles
         outcomes to be concerned about.
         '''
-        assert sleep_in(True, True) = True # weekday but on vacation -> sleep in 
-        assert sleep_in(True, False) = False # weekday but NOT on vacation -> do NOTsleep in 
-        assert sleep_in(False, True) = True # weekday and on vacation -> sleep in 
-        assert sleep_in(False, False) = True # weekday but NOT on vacation -> sleep in 
+        assert sleep_in(True, True) == True # weekday but on vacation -> sleep in 
+        assert sleep_in(True, False) == False # weekday but NOT on vacation -> do NOTsleep in 
+        assert sleep_in(False, True) == True # weekday and on vacation -> sleep in 
+        assert sleep_in(False, False) == True # weekday but NOT on vacation -> sleep in 
